f87e922f2618d54cf6e283c22a7f9dc21042c228
sam laitha <samlaitha@sams-MacBook-Pro.local>
2020-05-22 17:53:41 -0700

    adding some files

--- src/pages/SamPage.js
+++ src/pages/SamPage.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import '../css/portfolio.css';
 import '../css/responsive.css';
+import '../css/ionicons.css';
 
 import picture from '../images/profile-1-250x250.jpg';
+
+import proj1 from '../images/portfolio-1-600x400.jpg';
+import proj2 from '../images/portfolio-7-600x800.jpg';
+import proj3 from '../images/portfolio-2-600x400.jpg';
+import proj4 from '../images/portfolio-8-300x400.jpg';
+import proj5 from '../images/portfolio-9-300x400.jpg';
 
 export default class SamPage extends React.Component {
     constructor(props) {
@@ -52,6 +59,8 @@ export default class SamPage extends React.Component {
                 {/* <!-- container --> */}
             </section>
             {/* <!-- intro-section --> */}
+
+
         </div>
         );
     }
