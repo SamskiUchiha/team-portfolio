c70ce75548a9e73b2ab63835386adcd2346bc74f
Antonio Vega <antoniovegajr@Antonios-MBP.attlocal.net>
2020-05-22 19:57:13 -0700

    displaying photo in home page. Added photos folder in src folder

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Logo from "../images/SchoolBackGroundPicture.jpg";
+
 
 export default class HomePage extends React.Component {
     render() {
         return(
-            <div>Home Page!</div>
+            <div>
+                
+                <img src={Logo} alt="website logo" height="100%" width="100%"/>
+            </div>
+            
         );
     }
 }
