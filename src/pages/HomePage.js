3e921f8b84fea31e0f0d9a650b55099f7a6d8304
Martin-52 <m.landin5258@gmail.com>
2020-06-02 15:57:48 -0700

    Added Card for the rest of the guys.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,19 @@ export default class HomePage extends React.Component {
         return(
             <>
                 <ProfileCard
+                    name='Antonio'
+                    description='Test Description to see if this works or not.'
+                />
+                <ProfileCard
                     name='Martin'
+                    description='Test Description to see if this works or not.'
+                />
+                <ProfileCard
+                    name='Sam'
+                    description='Test Description to see if this works or not.'
+                />
+                <ProfileCard
+                    name='Santiago'
                     description='Test Description to see if this works or not.'
                 />
             </>
